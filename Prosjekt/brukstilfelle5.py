6ea6bd51a68919eb98e06450746f3718ceae8a46
kaisa.l <kaisa.oyre.larsen@gmail.com>
2024-03-20 11:11:36 +0100

    brukstilfelle 5 finished

--- Prosjekt/brukstilfelle5.py
+++ Prosjekt/brukstilfelle5.py
@@ -1,8 +1,13 @@
-
-# def skuespillereIStykker(): #brukstilfelle 5
-#     c.execute(skuespillereIStykkerQuery)
-#     results = c.fetchall()
-#     for row in results:
-#         print(f"Teaterstykke: {row[0]}, Skuespiller: {row[1]}, Rolle: {row[2]}")
-# if __name__ == "__main__":
-#     skuespillereIStykker()
+import sqlite3
+from datetime import datetime
+from queries import skuespillereIStykkerQuery
+con = sqlite3.connect("teater.db")
+c = con.cursor()
+#antar siden det ikke står i oppgavebeskrivelsen at funksjonen skal ta inn noe, så skal alle skuespillere med tilhørende stykker og roller skrives ut hver gang
+def skuespillereIStykker(): #brukstilfelle 5
+    c.execute(skuespillereIStykkerQuery)
+    results = c.fetchall()
+    for row in results:
+        print(f"Teaterstykke: {row[0]}, Skuespiller: {row[1]}, Rolle: {row[2]}")
+if __name__ == "__main__":
+    skuespillereIStykker()
